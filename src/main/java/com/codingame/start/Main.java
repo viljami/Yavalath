8d533bcab78bbc5d1bf194a4cf75b2d299bf4fd1
Viljami Peltola <viljami.peltola@iki.fi>
2019-10-13 19:55:54 +0300

    Save game result.

--- src/main/java/com/codingame/start/Main.java
+++ src/main/java/com/codingame/start/Main.java
@@ -1,6 +1,14 @@
 package com.codingame.start;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import com.codingame.gameengine.runner.MultiplayerGameRunner;
+import com.codingame.gameengine.runner.dto.GameResult;
+import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +17,26 @@ public class Main {
         gameRunner.addAgent("python3 ../agent1.py", "Player 1");
         gameRunner.addAgent("python3 ../agent2.py", "Player 2");
 
-        gameRunner.start();
+        Main.saveResult(
+            gameRunner.simulate(),
+            "game-result.json"
+        );
+    }
+
+    private static void saveResult(GameResult result, String fileName) {
+      String json = new Gson().toJson(result);
+
+      Path tmpdir = Paths.get("./data"); // .resolve("codingame");
+      tmpdir.toFile(); // .mkdirs();
+
+      if (json != null) {
+        File game = tmpdir.resolve(fileName).toFile();
+
+        try (PrintWriter out = new PrintWriter(game)) {
+          out.println(json);
+        } catch (IOException e) {
+          throw new RuntimeException("Cannot generate the game file", e);
+        }
+      }
     }
 }
